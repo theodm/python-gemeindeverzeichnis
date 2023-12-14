63655e8c5c75d52c41b2a111cba4f47f008773c3
Theodor Diesner-Mayer <theo.dm94@gmail.com>
2023-12-14 20:14:22 +0100

    snapshot of the data is now inside the repository

--- gemeindeverzeichnis.py
+++ gemeindeverzeichnis.py
@@ -1,6 +1,11 @@
+import os
+
 from GV100ADParser import GV100ADParser
 
-def load_gemeindeverzeichnis(filename="data/GV100AD_301123.txt", ignore_redundant_gemeindeverbaende=True):
+def load_gemeindeverzeichnis(filename: str | None=None, ignore_redundant_gemeindeverbaende=True):
+    if filename is None:
+        filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data", "GV100AD_301123.txt")
+
     reader = GV100ADParser(filename, ignore_redundant_gemeindeverbaende)
 
     return reader.read()
